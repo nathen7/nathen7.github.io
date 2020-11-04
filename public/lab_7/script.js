e1f2abcc9e83b0a103eb5305121bed80a80bb831
Nathan Huang <nathen7@umd.edu>
2020-11-04 01:39:00 -0500

    Lab 7

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,5 +1,17 @@
 function convertRestaurantsToCategories(restaurantList) {
   // process your restaurants here!
+  const list = restaurantList.reduce((collection, item, i) => {
+    const findCat = collection.find((findItem) => findItem.label === item.category);
+    if (!findCat) {
+      collection.push({
+        label: item.category,
+        y: 1
+      });
+    } else {
+      findCat.y += 1;
+    }
+    return collection;
+  }, []);
   return list;
 }
 
@@ -7,13 +19,18 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
   CanvasJS.addColorSet('customColorSet1', [
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+    "#2F4F4F",
+    "#008080",
+    "#2E8B57",
+    "#3CB371",
+    "#90EE90"
   ]);
 
   return {
     animationEnabled: true,
     colorSet: 'customColorSet1',
     title: {
-      text: 'Change This Title'
+      text: 'Places to Eat Out in the Future'
     },
     axisX: {
       interval: 1,
@@ -22,9 +39,11 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
       gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
+      title: 'The Desire to not Starve 2020 (colorized)',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {customBreaks: [
+        {startValue: 40, endValue: 50, color: "red"}, {startValue: 85, endValue: 100, color: "red"}, {startValue: 140, endValue: 175, color: "red"}
+      ]} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import countries from './public/lab_6/countries.js';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -25,11 +25,11 @@ app.route('/api')
     console.log('GET request detected');
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     console.log('POST request detected');
-    console.log('Form data in req.body', req.body);
-
-    res.json(countries);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    res.json(json)
   });
 
 app.listen(port, () => {
